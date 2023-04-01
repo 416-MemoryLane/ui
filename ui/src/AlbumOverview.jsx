e9fbf0c005e7de14503cb2fc363b5fc90449dbab
derekwong <derekw1@students.cs.ubc.ca>
2023-04-01 14:18:36 -0700

    more aesthetics

--- ui/src/AlbumOverview.jsx
+++ ui/src/AlbumOverview.jsx
@@ -10,16 +10,16 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 export const AlbumOverview = ({ title, images, onBackClick }) => {
-    const thumbnails = useMemo(() => {
-        return images.map((image, i) => {
-        let url = `http://localhost:4321${image}`;
-        const img = new Image();
-        img.src = url;
-        const width = img.width;
-        const height = img.height;
-        return { src: url, width: width, height: height };
-        });
-    }, [images]);
+  const thumbnails = useMemo(() => {
+    return images.map((image, i) => {
+      let url = `http://localhost:4321${image}`;
+      const img = new Image();
+      img.src = url;
+      const width = img.width;
+      const height = img.height;
+      return { src: url, width: width, height: height };
+    });
+  }, [images]);
 
   const photos = useMemo(() => {
     return images.map((image, i) => {
@@ -56,46 +56,50 @@ export const AlbumOverview = ({ title, images, onBackClick }) => {
 
   const albumContainer = ({ containerProps, children, containerRef }) => (
     <div
-        style={{
-            border: "2px solid #ccc",
-            borderRadius: "10px",
-            padding: "20px",
-        }}
+      style={{
+        border: "2px solid #ccc",
+        borderRadius: "10px",
+        padding: "20px",
+      }}
     >
-        <div ref={containerRef} {...containerProps}>
-            {children}
-        </div>
+      <div ref={containerRef} {...containerProps}>
+        {children}
+      </div>
     </div>
-);
+  );
 
-  const photoFrame = ({ layout, layoutOptions, imageProps: { alt, style, ...restImageProps } }) => (
+  const photoFrame = ({
+    layout,
+    layoutOptions,
+    imageProps: { alt, style, ...restImageProps },
+  }) => (
     <div
-        style={{
-            border: "2px solid #eee",
-            borderRadius: "4px",
-            boxSizing: "content-box",
-            alignItems: "center",
-            width: style?.width,
-            padding: `20px`,
-            margin: "5px",
-        }}
+      style={{
+        border: "2px solid #eee",
+        borderRadius: "4px",
+        boxSizing: "content-box",
+        alignItems: "center",
+        width: style?.width,
+        padding: `20px`,
+        margin: "5px",
+      }}
     >
-        <img alt={alt} style={{ ...style, width: "100%"}} {...restImageProps} />
+      <img alt={alt} style={{ ...style, width: "100%" }} {...restImageProps} />
     </div>
-);
+  );
 
   return (
     <div className="mx-auto px-8 sm:px-10 lg:px-12">
       <div className="flex justify-between items-center">
-        <div className="flex">
-          <h1 className="text-2xl font-bold">{title}</h1>
-          <button className="px-10" onClick={handleFileUpload}>
-            <span className="text-sm font-semibold">Add Photo</span>
-          </button>
-        </div>
-        <div className="flex items-center">
-          <CloseIcon className="" onClick={onBackClick}></CloseIcon>
-        </div>
+        <button className="px-3 hover:text-sky-400" onClick={handleFileUpload}>
+          <span className="text-md font-semibold">Add Photo</span>
+        </button>
+        <h1 className="text-2xl font-bold">{title}</h1>
+
+        <CloseIcon
+          className="hover:fill-current hover:text-sky-400"
+          onClick={onBackClick}
+        ></CloseIcon>
       </div>
       <PhotoAlbum
         photos={thumbnails}
