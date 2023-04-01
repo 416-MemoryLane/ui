cd656948d55d15fb88934f7b3971a7d0b37258b3
derekwong <derekw1@students.cs.ubc.ca>
2023-04-01 00:28:44 -0700

    implement add image

--- server/app.mjs
+++ server/app.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import fs from "fs";
 import cors from "cors";
 
+import multer from "multer";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -30,6 +32,29 @@ app.get("/albums/:id", (req, res) => {
   const { id } = req.params;
   console.log(id);
   res.send(id);
+});
+
+app.post("/albums/:albumName/images", (req, res) => {
+  const { albumName } = req.params;
+  var albumPath = `${ALBUM_DIR}/${albumName}`;
+  if (!fs.existsSync(albumPath)) {
+    fs.mkdirSync(albumPath);
+  }
+  var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, albumPath);
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    },
+  });
+  var upload = multer({ storage: storage }).single("myfile");
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded successfully!");
+  });
 });
 
 app.listen(port, () => {
--- ui/src/AlbumOverview.jsx
+++ ui/src/AlbumOverview.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo} from "react";
+import { useState, useMemo } from "react";
 import { CloseIcon } from "./Icons/CloseIcon";
 import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
@@ -12,8 +12,8 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 export const AlbumOverview = ({ title, images, onBackClick }) => {
   const thumbnails = useMemo(() => {
     return images.map((image, i) => {
-        let url = `http://localhost:4321${image}`;
-        return { src: url, width: 150, height: 100};
+      let url = `http://localhost:4321${image}`;
+      return { src: url, width: 150, height: 100 };
     });
   }, []);
 
@@ -25,14 +25,63 @@ export const AlbumOverview = ({ title, images, onBackClick }) => {
 
   const [index, setIndex] = useState(-1);
 
+  const addImage = async (imagePath, imageDataUrl) => {
+    const res = await fetch(`http://localhost:4321/albums/${title}/images`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({imagePath, imageDataUrl}),
+    });
+    if (res.ok) {
+      console.log("Image added");
+    } else {
+      console.log("Error adding image");
+    }
+  };
+
+  const handleFileUpload = async () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.onchange = async (event) => {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('myfile', file);
+      try {
+        const response = await fetch(`http://localhost:4321/albums/${title}/images`, {
+          method: 'POST',
+          body: formData,
+        });
+        const data = await response.text();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    input.click();
+  };
+  
+  
+
   return (
-    <div className="container mx-auto">
-      <CloseIcon onClick={onBackClick}></CloseIcon>
-      <PhotoAlbum
-        photos={thumbnails}
-        layout="rows"
-        onClick={({ index }) => setIndex(index)}
-      />
+    <div className="mx-auto px-8 sm:px-10 lg:px-12">
+      <div className="flex justify-between items-center">
+        <div className="flex">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <button className="px-10" onClick={handleFileUpload}>
+            <span className="text-sm font-semibold">Add Photo</span>
+          </button>
+        </div>
+        <div className="flex items-center">
+          <CloseIcon className="" onClick={onBackClick}></CloseIcon>
+        </div>
+      </div>
+        <PhotoAlbum
+          photos={thumbnails}
+          layout="rows"
+          onClick={({ index }) => setIndex(index)}
+        />
       <Lightbox
         slides={photos}
         open={index >= 0}
